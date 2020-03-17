02839729a814c923fa86346e518ede8bfd6279ae
Agrover112 <agrover112@gmail.com>
2020-03-17 20:13:23 +0530

    Added game file

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#File:guessinggame.sh
+
+function guessing_game()
+{
+local count=$(ls | wc -l)
+while  true;
+do
+ read -p "Enter your guess :" mg
+ if [[ $mg -lt $count ]]
+ then
+    echo "guessed num  LESSER than true num"
+ elif [[ $mg -gt $count ]]
+ then
+    echo "guessed num GREATER than true num"
+ else
+    echo "CONGRATULATIONS! Right guess! "
+break
+ fi
+done
+}
+echo "Let the guessin game begin..."
+guessing_game
